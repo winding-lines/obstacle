85e8ad9f55f00178f73371e7fbb9d601143d677b
Marius S <39998+winding-lines@users.noreply.github.com>
2023-06-28 08:14:22 +0300

    Wrap initial apis

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-use memmap2;
+use memmap2::{self, MmapAsRawDesc, MmapOptions};
+use std::io::Result;
+use std::ops::Deref;
 
 /// Wrapped for the memmap2::Mmap.
-pub struct Mmap {
-    inner: memmap2::Mmap,
+pub struct Mmap(memmap2::Mmap);
+
+impl Mmap {
+    pub unsafe fn map<T: MmapAsRawDesc>(file: T) -> Result<Mmap> {
+        Ok(Mmap(MmapOptions::new().map(file)?))
+    }
+}
+
+impl AsRef<[u8]> for Mmap {
+    #[inline]
+    fn as_ref(&self) -> &[u8] {
+        self.0.deref()
+    }
 }
