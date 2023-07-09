aa68ff3b03c8dcba1f2214743f895a0abaa37276
Marius S <39998+winding-lines@users.noreply.github.com>
2023-07-09 07:38:25 -0700

    Fix examples

--- examples/local.rs
+++ examples/local.rs
@@ -1,8 +1,8 @@
-use obstinate;
+use obstacle;
 use std::{fs::File, str::from_utf8};
 
 pub fn main() {
     let file = File::open("examples/files/hello_world.txt").unwrap();
-    let mmaped = unsafe { obstinate::Mmap::map(&file) }.unwrap();
+    let mmaped = unsafe { obstacle::Mmap::map(&file) }.unwrap();
     print!("content: {}.", from_utf8(&mmaped[..]).unwrap());
 }
--- examples/minio.rs
+++ examples/minio.rs
@@ -1,4 +1,11 @@
-//! This example assumes that you have installed minio locally.
+//! Download a file from minio (S3 API compatible) and print its content.
+//!
+//! This example assumes that:
+//! 1. you have installed minio locally.
+//! 2. you have created a bucket named "one" in minio.
+//! 3. you have uploaded a file named "foods2.csv" to the bucket.
+//! 4. you have setup the access key and secret key for minio and saved it under ~/.aws/credentials.
+//!
 //! Run with:
 //!     cargo run --example   minio --features aws
 
@@ -6,14 +13,14 @@
 fn mmap_from_url() {
     use awscreds::Credentials;
     use object_store::ClientConfigKey;
-    use obstinate::set_cloud_options;
-    use obstinate::AmazonS3ConfigKey as Key;
+    use obstacle::set_cloud_options;
+    use obstacle::AmazonS3ConfigKey as Key;
     use std::str::from_utf8;
 
     let cred = Credentials::default().unwrap();
 
     // Propagate the credentials and other cloud options.
-    let cloud_options = obstinate::CloudOptions::default().with_aws([
+    let cloud_options = obstacle::CloudOptions::default().with_aws([
         (Key::AccessKeyId, &cred.access_key.unwrap()),
         (Key::SecretAccessKey, &cred.secret_key.unwrap()),
         (Key::Region, &"us-east-1a".into()),
@@ -21,7 +28,7 @@ fn mmap_from_url() {
         (Key::Endpoint, &"http://localhost:9000".into()),
     ]);
     set_cloud_options(cloud_options);
-    let mmaped = obstinate::Mmap::from_url(&"s3://one/foods2.csv").unwrap();
+    let mmaped = obstacle::Mmap::from_url(&"s3://one/foods2.csv").unwrap();
     print!("content: {}.", from_utf8(&mmaped[..]).unwrap());
 }
 
